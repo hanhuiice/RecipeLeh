8a6aa538c585b4eaba17b9fd377e133b294d1a14
Joseph <josephleejj@hotmail.sg>
2021-05-31 15:11:47 +0800

    #6

---
+++ lib/models/username.dart
@@ -0,0 +1,6 @@
+class userName {
+
+  final String uid;
+  userName({required this.uid});
+
+}
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,16 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:recipe_leh/models/username.dart';
 
 class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // sign in anon
+  // create user object based on firebased user
+  userName? _fromFirebaseUser(User user)
+  {
+  return user != null ? userName (uid: user.uid) : null;
+  }
+
+
+  // Login Anon
   Future signInAnon() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
-      return user;
-    } catch (e) {
+      return _fromFirebaseUser(user!);
+    } catch(e){
       print(e.toString());
       return null;
     }
